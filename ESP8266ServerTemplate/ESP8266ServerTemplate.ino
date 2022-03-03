9204eba3c601304a2c324880b24938e00c21c10e
世元 李 <1498045907@qq.com>
2022-03-03 14:06:11 +0800

    Rrfresh

--- ESP8266ServerTemplate/ESP8266ServerTemplate.ino
+++ ESP8266ServerTemplate/ESP8266ServerTemplate.ino
@@ -1,3 +1,6 @@
+#pragma region Head
+
+
 #include <Wire.h> 
 
 #include <ESP8266Wifi.h>
@@ -17,11 +20,11 @@
 #include <stdio.h>
 
 #ifndef STASSID
-#define STASSID "HUAWEI0260W"
-#define STAPSK  "HUAWEI666"
+#define STASSID "lishiyuan"
+#define STAPSK  "761834925"
 #endif	
 
-#define PORT 32212
+#define PORT 25523
 #define SERIAL_BAUD 115200
 
 U8G2_SH1106_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
@@ -35,7 +38,133 @@ WiFiServer Server(PORT);
 WiFiClient Client;
 char IPStr[30];
 
-void FlushDisplay(char **lines, int count)
+void SetupU8g2();
+void FlushDisplay(char** lines, int count);
+void SetupWiFi();
+
+#pragma endregion
+
+#pragma region DFS
+
+#define DW(p,v) digitalWrite(p,v)
+#define DR(p) digitalRead(p)
+
+#define AR(p) analogRead(p)
+#define AW(p,v) analogWrite(p,v)
+
+#define PM(p,m) pinMode(p,m)
+
+#pragma endregion
+
+
+#pragma region Lines
+typedef struct _Lines
+{
+	char *L1;
+	char *L2;
+	char *L3;
+	char *L4;
+	char *L5;
+}Lines, * PLines;
+PLines Data = NULL;
+
+void U8g2Println(const char*l)
+{
+	int x = 0;
+	char* i = (char*)Data;
+	for (; x < 5; i += sizeof(char*), x++)
+	{
+		if (strlen(i) == 0) 
+		{ strcpy(i, l);
+		char* mm[] = { Data->L1 ,Data->L2,Data->L3,Data->L4,Data->L5 };
+		FlushDisplay(mm, 5);
+
+		return; }
+	}
+
+	char* temp = Data->L1;
+	Data->L1 = Data->L2;
+	Data->L2 = Data->L3;
+	Data->L3 = Data->L4;
+	Data->L4 = Data->L5;
+
+	strcpy(temp, l);
+
+	Data->L5 = temp;
+
+	char* mm[] = { Data->L1 ,Data->L2,Data->L3,Data->L4,Data->L5 };
+	FlushDisplay(mm, 5);
+
+}
+
+#pragma endregion
+
+
+void SoftAPInit()
+{
+	/// <summary>
+	WiFi.mode(WiFiMode::WIFI_AP);
+
+	IPAddress softLocal(192, 168, 1, 1);   // 1 设置内网WIFI IP地址
+	IPAddress softGateway(192, 168, 1, 1);
+	IPAddress softSubnet(255, 255, 255, 0);
+	WiFi.softAPConfig(softLocal, softGateway, softSubnet);
+
+	String apName = "Acite_White_Host";  // 2 设置WIFI名称
+	const char* softAPName = apName.c_str();
+
+	WiFi.softAP(softAPName, "11223344");      // 3创建wifi  名称 +密码 adminadmin
+
+	U8g2Println(softAPName);
+	U8g2Println("11223344");
+
+	Serial.println("Setuped SoftAP.");
+	/// </summary>
+}
+
+void setup() {
+	Serial.begin(SERIAL_BAUD);
+	SetupU8g2();
+	//SetupWiFi();
+	SoftAPInit();
+	PM(D4, OUTPUT);
+	DW(D4, HIGH);
+	
+
+
+	strcpy(IPStr, WiFi.localIP().toString().c_str());
+
+	Server.begin();
+
+}
+int xxb = 0;
+void loop() {
+	xxb++;
+	char bb[10];
+	itoa(xxb, bb, 10);
+	Serial.println(bb);
+	U8g2Println(bb);
+	if (!Client.connected()) {
+		digitalWrite(D4, LOW);
+		if (Server.hasClient())
+		{
+			Client = Server.available();
+			digitalWrite(D4, HIGH);
+		}
+		else
+			return;
+	}
+	String dd = Client.readStringUntil('\0');
+	Client.println("OK");
+}
+
+
+void Reverse(uint8_t pin)
+{
+	digitalWrite(pin, !digitalRead(pin));
+}
+
+void FlushDisplay(char** lines, int count)
 {
 	u8g2.clearBuffer();
 	for (int i = 0, y_base = 12; i < count; i++, y_base += 11)
@@ -45,65 +174,26 @@ void FlushDisplay(char **lines, int count)
 	u8g2.sendBuffer();
 }
 
-void setup() {
+void SetupU8g2()
+{
+	Data = (PLines)malloc(sizeof(Lines));
+	memset(Data, 0, sizeof(Lines));
+	Data->L1 = new char[32];
+	Data->L2 = new char[32];
+	Data->L3 = new char[32];
+	Data->L4 = new char[32];
+	Data->L5 = new char[32];
+
 	u8g2.begin();
-	Serial.begin(SERIAL_BAUD);
 	u8g2.setFontMode(1);	// Transparent
 	u8g2.setFontDirection(0);
-	u8g2.setFont(u8g2_font_6x12_tf);
+	u8g2.setFont(u8g2_font_6x13_tf);
+}
 
-	pinMode(A0, INPUT);
-	pinMode(D4, OUTPUT);
-
+void SetupWiFi()
+{
 	wifi_station_set_hostname("Acite_White_Host");
-	digitalWrite(D4, LOW);
 
 	WiFi.mode(WIFI_STA);
 	WiFi.begin(ssid, password);
-
-	char* n1[] = { "Connecting......" };
-	
-	FlushDisplay(n1, 1);
-
-	while (WiFi.status() != WL_CONNECTED) {
-		delay(200);
-		Reverse(D4);
-	}
-
-	digitalWrite(D4, LOW);
-	strcpy(IPStr, WiFi.localIP().toString().c_str());
-
-	char* n2[] = { "Connected.",IPStr,"Client Disconnected!" };
-	FlushDisplay(n2, 3);
-	Server.begin();
-
-}
-
-void loop() {
-	int bb = analogRead(A0);
-
-	if (!Client.connected()) {
-		digitalWrite(D4, LOW);
-		char* n2[] = { "Connected.",IPStr,"Client Disconnected!" };
-		FlushDisplay(n2, 3);
-		if (Server.hasClient())
-		{
-			Client = Server.available();
-			digitalWrite(D4, HIGH);
-			char* n3[] = { "Connected.",IPStr,"Client Connected!" };
-			FlushDisplay(n3, 3);
-		}
-		else
-			return;
-	}
-
-
-	//Client.print(buf);
-	//Client.readStringUntil('\n');
-}
-
-
-void Reverse(uint8_t pin)
-{
-	digitalWrite(pin, !digitalRead(pin));
 }
